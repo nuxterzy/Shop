8fcec4418775adfa03c42a8d79f7a770cea0aca3
Łukasz Fujarski <lukasion@gmail.com>
2023-05-27 11:41:30 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const test = ref('')
+const products = ref([])
+
+const addProduct = () => {
+    products.value.push(test.value)
+}
+
+</script>
+
+<template>
+    <p>Twoja wiadomość: {{ test }}</p>
+
+    <input v-model="test" type="text" />
+    <button @click.prevent="addProduct">Dodaj</button>
+
+    <p><strong>Lista produktow:</strong></p>
+    <ul>
+        <li v-for="item in products">
+            {{ item }}
+        </li>
+    </ul>
+</template>
